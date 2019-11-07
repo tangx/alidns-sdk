26963f125cbafd4d5bb03f479b5026b3175d2967
tangxin <tangxin@tangxin-mbp.local>
2019-11-08 00:20:35 +0800

    new: 增加 4 个 record 操作

--- record.go
+++ record.go
@@ -26,6 +26,8 @@ func (cli *Client) AddDomainRecord(domain string, RR string, Type string, Value 
 		RecordID  string
 		RequestID string
 	}
+
+	// 请求
 	err := cli.Do("AddDomainRecord", body, &respInfo)
 	if err != nil {
 		return 0, err
@@ -34,4 +36,91 @@ func (cli *Client) AddDomainRecord(domain string, RR string, Type string, Value 
 	//fmt.Println(respInfo)
 	rrID, err := strconv.Atoi(respInfo.RecordID)
 	return rrID, nil
+}
+
+// DeleteDomainRecord 调用DeleteDomainRecord根据传入参数删除解析记录
+// https://help.aliyun.com/document_detail/29773.html?spm=a2c4g.11186623.6.642.701b2846ThgD9h
+func (cli *Client) DeleteDomainRecord(RecordId int) (bool, error) {
+	// 设置必要参数
+	body := map[string]string{
+		"RecordId": strconv.Itoa(RecordId),
+	}
+
+	var respInfo struct {
+		RecordID  string
+		RequestID string
+	}
+
+	err := cli.Do("DeleteDomainRecord", body, &respInfo)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+// UpdateDomainRecord 调用UpdateDomainRecord根据传入参数修改解析记录。
+// https://help.aliyun.com/document_detail/29774.html?spm=a2c4g.11186623.6.643.1f743192JMf3pj
+func (cli *Client) UpdateDomainRecord(RR string, RecordId int, Type string, Value string, data map[string]string) (bool, error) {
+
+	body := map[string]string{
+		"RR":       RR,
+		"RecordId": strconv.Itoa(RecordId),
+		"Type":     strings.ToUpper(Type),
+		"Value":    Value,
+	}
+
+	for k, v := range data {
+		body[k] = v
+	}
+	var respInfo struct {
+		RecordID  string
+		RequestID string
+	}
+
+	err := cli.Do("UpdateDomainRecord", body, &respInfo)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+// DescribeDomainRecords 调用DescribeDomainRecords根据传入参数获取指定主域名的所有解析记录列表。
+// https://help.aliyun.com/document_detail/29776.html?spm=a2c4g.11186623.6.638.f7553b59curplN
+func (cli *Client) DescribeDomainRecords(domain string, data map[string]string) (interface{}, error) {
+	body := map[string]string{
+		"DomainName": domain,
+	}
+
+	for k, v := range data {
+		body[k] = v
+	}
+
+	var respInfo struct {
+		DomainRecords struct {
+			Record []struct {
+				DomainName string `json:"DomainName"`
+				Line       string `json:"Line"`
+				Locked     bool   `json:"Locked"`
+				Priority   int    `json:"Priority"`
+				RR         string `json:"RR"`
+				RecordID   string `json:"RecordId"`
+				Status     string `json:"Status"`
+				TTL        int    `json:"TTL"`
+				Type       string `json:"Type"`
+				Value      string `json:"Value"`
+			} `json:"Record"`
+		} `json:"DomainRecords"`
+		PageNumber int    `json:"PageNumber"`
+		PageSize   int    `json:"PageSize"`
+		RequestID  string `json:"RequestId"`
+		TotalCount int    `json:"TotalCount"`
+	}
+
+	err := cli.Do("DescribeDomainRecords", body, &respInfo)
+	if err != nil {
+		return nil, err
+	}
+	return respInfo, nil
 }
--- record_test.go
+++ record_test.go
@@ -3,20 +3,54 @@ package alidns
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestClient_AddDomainRecord(t *testing.T) {
 
 	cli := New(akid, akey)
 
-	RR := "tangx123"
+	RR := "tangx1222323"
 	Type := "A"
 	Value := "123.231.12.11"
 
+	// add
 	rrId, err := cli.AddDomainRecord("rockontrol.com", RR, Type, Value, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	fmt.Printf("RequestId: %d \n", rrId)
+
+	time.Sleep(3 * time.Second)
+
+	// update
+	ok, err := cli.UpdateDomainRecord(RR, rrId, Type, "13.131.12.11", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if ok {
+		fmt.Println("update success")
+	} else {
+		fmt.Println("update faild")
+	}
+
+	time.Sleep(3 * time.Second)
+
+	// delete
+	ok, err = cli.DeleteDomainRecord(rrId)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if ok {
+		fmt.Println("delete success")
+	} else {
+		fmt.Println("delete faild")
+	}
+}
+
+func TestClient_DescribeDomainRecords(t *testing.T) {
+	cli := New(akid, akey)
+
+	data, _ := cli.DescribeDomainRecords("rockontrol.com", nil)
+	fmt.Println(data)
 }
