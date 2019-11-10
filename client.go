e5c13519c47e978523e227844fd92b6aef25904d
tangxin <tangxin@tangxin-mbp.local>
2019-11-10 13:49:05 +0800

    增加 comment

--- client.go
+++ client.go
@@ -1,10 +1,12 @@
 package alidns
 
+// Client for alidns config
 type Client struct {
 	SecretID  string
 	SecretKey string
 }
 
+// New return a alidns client
 func New(secretID, secretKey string) *Client {
 	return &Client{
 		SecretID:  secretID,
